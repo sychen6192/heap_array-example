16f788cee494b635f0ac1b2e9e10a324f16f03c4
sychen6192 <32225054+sychen6192@users.noreply.github.com>
2019-09-04 15:41:42 +0800

    v3

--- 0904.cpp
+++ 0904.cpp
@@ -7,20 +7,21 @@ int main()
 int **data;
 int data_height, data_width;
 
-cout << "¿é¤J¤Gºû°}¦Cºû«×:";
+// declare array size
+cout << "Please input 2 dimention array :";
 cin >> data_height >> data_width;
 data = new int*[data_height];
 for(int i = 0; i < data_height; i++)
     data[i] = new int[data_width];
 
-
+// assign a summation for array
 for(int i=0;i < data_height; i++){
     for(int j=0;j < data_width; j++){
          data[i][j]=i + j;
          cout << "i=" << i <<" j=" << j << " i+j=" << data[i][j] << endl;
     }
 }
-
+// release allocate memory
 for(int i = 0; i < data_height; i++){
     delete [] data[i];
 delete [] data;
